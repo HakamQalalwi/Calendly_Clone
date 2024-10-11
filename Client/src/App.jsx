58d7b5064324d1aea3c75e886911afb3792f9044
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-10-11 21:06:26 +0300

    Initialize teams page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import {TeamsPage} from "./Pages/Teams/TeamsPage.jsx";
 
 function App() {
   return (
     <>
       <p>Hello From React App</p>
+        {/*<TeamsPage />*/}
     </>
   );
 }
